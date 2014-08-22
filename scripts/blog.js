881b049a66d04fc8ad85a40561017a8b1dad801e
suninf <sunjiangwei@yeah.net>
2014-08-23 07:30:44 +0800

    update

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,31 +1,20 @@
-function addLoadEvent(func) {
-    var oldload = window.onload;
-    if (typeof window.onload != 'function') {
-        window.onload = func;
-    }
-    else {
-        window.onload = function () {
-            oldload();
-            func();
+
+// $(document).ready()
+$(function(){
+    // animate bar function
+    function animateBar($item,noAni){
+        var spanLeft = $item.find('span').offset().left;
+        var conLeft = $item.parent().offset().left;
+        var left = spanLeft - conLeft - 4;
+        var width = $item.find('span').width() + 8;
+
+        if(noAni){
+            $('#cateBar').css({left:left,width:width})
+        }else{
+            $('#cateBar').stop().animate({left:left,width:width},300)
         }
     }
-}
-
-// animate bar function
-function animateBar($item,noAni){
-	var spanLeft = $item.find('span').offset().left;
-	var conLeft = $item.parent().offset().left;
-	var left = spanLeft - conLeft - 4;
-	var width = $item.find('span').width() + 8;
-
-	if(noAni){
-		$('#cateBar').css({left:left,width:width})
-	}else{
-		$('#cateBar').stop().animate({left:left,width:width},300)
-	}
-}
-
-$(function(){
+    
 	var waitForFinalEvent = (function () {
 		var timers = {};
 		return function (callback, ms, uniqueId) {
@@ -51,34 +40,27 @@ $(function(){
 		})
 	})
 
+    // bar status depends on location.pathname
+    (function naviBarInit() {
+        // remove class 'on'
+        $(".artical-cate li").removeClass( "on" );
+
+        // parse url
+        var path_arg = location.pathname;
+
+        if( path_arg == "/" ) {
+            $("#nav_home_id").addClass("on");
+        } else if( path_arg.indexOf("about.html") >= 0 ) {
+            $("#nav_about_id").addClass("on");
+        } else if( path_arg.indexOf("works.html") >= 0 ) {
+            $("#nav_works_id").addClass("on");
+        } else {
+            // articles.html or posts
+            $("#nav_articles_id").addClass("on");
+        }
+
+        $('.cate-bar').css( {visibility:"visible"} );
+        animateBar($('.artical-cate .on'),true);
+    })();
+
 });
-
-// bar status depends on location.pathname
-function naviBarInit() {
-    // remove class 'on'
-	$(".artical-cate li").removeClass( "on" );
-
-    // parse url
-    var path_arg = location.pathname;
-
-    if( path_arg == "/" ) {
-        $("#nav_home_id").addClass("on");
-    } else if( path_arg.indexOf("about.html") >= 0 ) {
-        $("#nav_about_id").addClass("on");
-    } else if( path_arg.indexOf("works.html") >= 0 ) {
-        $("#nav_works_id").addClass("on");
-    } else {
-        // articles.html or posts
-        $("#nav_articles_id").addClass("on");
-    }
-
-    $('.cate-bar').css( {visibility:"visible"} );
-    animateBar($('.artical-cate .on'),true);
-}
-
-
-// window.onload
-// addLoadEvent( naviBarInit );
-
-// operator after DOM finished, may be faster
-$(document).ready( naviBarInit );
