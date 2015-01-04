4b1baa5702a1139e3f4698b2aed28ac354bfb214
zhenshan <zhenshan@taobao.com>
2015-01-04 22:33:13 +0800

    update js

--- scripts/blog.js
+++ scripts/blog.js
@@ -11,17 +11,11 @@ $(function(){
             var target = e.target;
 
             if(!checkbox.checked || sidebar.contains(target) || (target === checkbox || target === toggle)) {
-                localStorage.sidebar_checked = checkbox.checked ? '1' : '0';
                 return;
             }
 
-            localStorage.sidebar_checked = '0';
             checkbox.checked = false;
         }, false);
     })(document);
     
-    // load last remember status
-    if ( localStorage.sidebar_checked == '1' ) {
-        $(".sidebar-checkbox").prop('checked', true);
-    }
 });
